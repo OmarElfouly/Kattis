a9bc4530048d5c40d2d51372268d423879a8f71b
omarelfouly <omarelfouly@aucegypt.edu>
2023-10-26 14:15:42 +0300

    Better Moscow Dream

--- moscowDream.cpp
+++ moscowDream.cpp
@@ -3,11 +3,7 @@ using namespace std;
 
 int main(){
     int a,b,c,n;
-    cin >> a >> b >> c >> n;
-    if(a>=1 && b>=1 && c>=1 && a+b+c>=n && n>=3){
-        cout << "YES" << endl;
-    }else{
-        cout << "NO" << endl;
-    }
+    scanf("%d %d %d %d", &a, &b, &c, &n);
+    printf((a>=1 && b>=1 && c>=1 && a+b+c>=n && n>=3)? "YES\n" : "NO\n");
     return 0;
 }
